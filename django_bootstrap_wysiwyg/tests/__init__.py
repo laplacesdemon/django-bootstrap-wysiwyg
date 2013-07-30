2aa730e2f01c18910d1ba0eccff011a6335689d4
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2013-07-30 12:10:38 +0300

    testing with nosetests

--- django_bootstrap_wysiwyg/tests/__init__.py
+++ django_bootstrap_wysiwyg/tests/__init__.py
@@ -1,1 +1,17 @@
+from django.conf import settings
+
+
+settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    },
+    INSTALLED_APPS=[
+        'django_bootstrap_wysiwyg',
+    ]
+)
+
+
 from .widgets import *
